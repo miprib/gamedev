f50ed68dcc15037511391823929ad9f4fa2420c3
Martisius <martisiuslukas97@gmail.com>
2019-11-26 00:05:53 +0200

    Added winning scene

--- OnlyOne/Assets/Scripts/EndScript.cs
+++ OnlyOne/Assets/Scripts/EndScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndScript : MonoBehaviour
 {
@@ -17,11 +18,15 @@ public class EndScript : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == redPlayer.tag ) {
+            ProgressInformation.winnerPlayerTag = redPlayer.tag.ToString();
+            SceneManager.LoadScene("WinningScene");
             Debug.Log("Red won");
         }
 
         if (collision.gameObject.tag == bluePlayer.tag)
         {
+            ProgressInformation.winnerPlayerTag = bluePlayer.tag.ToString();
+            SceneManager.LoadScene("WinningScene");
             Debug.Log("Blue won");
         }
     }
---
+++ OnlyOne/Assets/Scripts/WinnerPageScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinnerPageScript : MonoBehaviour
+{
+    public Text winnerTitle;
+    public Image winnerImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ProgressInformation.winnerPlayerTag == null)
+        {
+            return;
+        }
+        if (ProgressInformation.winnerPlayerTag.ToString() == "BluePlayer") {
+            winnerTitle.text = "Blue player won!";
+            winnerImage.sprite = Resources.Load<Sprite>("FaceNinja");
+        } else {
+            winnerTitle.text = "Red player won!";
+            winnerImage.sprite = Resources.Load<Sprite>("FaceMask");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //SceneManager.LoadScene("MainMenu");
+            Debug.Log("Open main menu");
+        }
+    }
+}
