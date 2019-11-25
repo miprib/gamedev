c2638720fbf25f75fb213038bbd47b1612728eba
Mikas <mikas.prib@gmail.com>
2019-11-25 22:09:22 +0200

    In-game UI and victory cup
    Added In-game UI. TODO: needs to respond to banana throw and pickup (by removing/adding the banana).
    
    Added victory cup. TODO: needs to register game end.

---
+++ OnlyOne/Assets/Scripts/EndScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScript : MonoBehaviour
+{
+
+    public Transform redPlayer;
+    public Transform bluePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == redPlayer.tag ) {
+            Debug.Log("Red won");
+        }
+
+        if (collision.gameObject.tag == bluePlayer.tag)
+        {
+            Debug.Log("Blue won");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
