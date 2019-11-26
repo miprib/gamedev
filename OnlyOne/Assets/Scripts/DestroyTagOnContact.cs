18af4736be6ec672641ea756ad890bef1fed53d9
Mikas <mikas.prib@gmail.com>
2019-11-26 22:09:07 +0200

    Victory cup changes
    Victory cup now falls down when the golden block is touched by any player.

---
+++ OnlyOne/Assets/Scripts/DestroyTagOnContact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTagOnContact : MonoBehaviour
+{
+    public String tagToDestroy;
+    public Transform redPlayer;
+    public Transform bluePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == redPlayer.tag ||
+            collision.gameObject.tag == bluePlayer.tag)
+        {
+            Destroy(GameObject.FindWithTag(tagToDestroy));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
